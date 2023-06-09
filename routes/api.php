aa2d2c437e7625790c35970288d037b9670b1c93
Tiago Pereira <pereiratiaggo@gmail.com>
2023-06-09 17:20:30 -0300

    aula41 + vite npm run buid

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Api\Auth\AuthController;
 use App\Http\Controllers\Api\SupportController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
@@ -15,8 +16,17 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+
+Route::post('/login', [AuthController::class, 'auth']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::apiResource('/supports', SupportController::class);
+});
+
+/*
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
-
-Route::apiResource('/supports', SupportController::class);
+*/
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Enums\SupportStatus;
 use App\Http\Controllers\Admin\{SupportController};
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\Site\SiteController;
 use Illuminate\Support\Facades\Route;
 
@@ -9,17 +10,29 @@ Route::get('/test', function () {
     dd(array_column(SupportStatus::cases(), 'name'));
 });
 
-Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
-Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
-Route::post('/supports/store', [SupportController::class, 'store'])->name('supports.store');
-
-Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
-Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
-Route::put('/supports/{id}', [SupportController::class, 'update'])->name('supports.update');
-Route::delete('/supports/{id}/delete', [SupportController::class, 'destroy'])->name('supports.destroy');
-
 Route::get('/contato', [SiteController::class, 'contact']);
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+
+    Route::get('/supports', [SupportController::class, 'index'])->name('supports.index');
+    Route::get('/supports/create', [SupportController::class, 'create'])->name('supports.create');
+    Route::post('/supports/store', [SupportController::class, 'store'])->name('supports.store');
+
+    Route::get('/supports/{id}', [SupportController::class, 'show'])->name('supports.show');
+    Route::get('/supports/{id}/edit', [SupportController::class, 'edit'])->name('supports.edit');
+    Route::put('/supports/{id}', [SupportController::class, 'update'])->name('supports.update');
+    Route::delete('/supports/{id}/delete', [SupportController::class, 'destroy'])->name('supports.destroy');
+});
+
+require __DIR__ . '/auth.php';
